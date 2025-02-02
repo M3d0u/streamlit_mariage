610e657abf9a708fa6d980593a7f92ad7ce266b3
M2dlgle <74049043+M3d0u@users.noreply.github.com>
2025-02-02 13:06:02 +0100

    Update camedeon.py

--- camedeon.py
+++ camedeon.py
@@ -84,38 +84,38 @@ col1, col_arrow1, col2, col_arrow2, col3, col_arrow3, col4, col_arrow4, col5 = s
 )
 
 with col1:
-    st.image("assets/eglise.svg", width=100)
-    st.subheader("16h")
-    st.markdown("Cérémonie religieuse")
+    st.image("assets/eglise.svg", width=150, caption="16h - Cérémonie religieuse")
+    #st.subheader("16h")
+    #st.markdown("Cérémonie religieuse")
 
 with col_arrow1:
     st.markdown("<h1 style='text-align: center;'>→</h1>", unsafe_allow_html=True)
 
 with col2:
-    st.image("assets/cocktail.svg", width=100)
-    st.subheader("18h")
-    st.markdown("Cocktail")
+    st.image("assets/cocktail.svg", width=150, caption="18h - Cocktail")
+    #st.subheader("18h")
+    #st.markdown("Cocktail")
 
 with col_arrow2:
     st.markdown("<h1 style='text-align: center;'>→</h1>", unsafe_allow_html=True)
 
 with col3:
-    st.image("assets/diner.svg", width=100)
-    st.subheader("20h30")
-    st.markdown("Diner")
+    st.image("assets/diner.svg", width=150, caption="20h30 - Dîner")
+    #st.subheader("20h30")
+    #st.markdown("Diner")
 
 with col_arrow3:
     st.markdown("<h1 style='text-align: center;'>→</h1>", unsafe_allow_html=True)
 
 with col4:
-    st.image("assets/soiree.svg", width=100)
-    st.subheader("23h")
-    st.markdown("Soirée")
+    st.image("assets/soiree.svg",  width=150, caption="23h - Soirée")
+    #st.subheader("23h")
+    #st.markdown("Soirée")
 
 with col_arrow4:
     st.markdown("<h1 style='text-align: center;'>→</h1>", unsafe_allow_html=True)
 
 with col5:
-    st.image("assets/brunch.svg", width=100)
-    st.subheader("11h")
-    st.markdown("Brunch")
+    st.image("assets/brunch.svg", width=150, caption="11h - Brunch")
+    #st.subheader("11h")
+    #st.markdown("Brunch")
