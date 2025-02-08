317514a1e946b35c89da2abb973229aa45011545
M2dlgle <74049043+M3d0u@users.noreply.github.com>
2025-02-08 16:58:51 +0100

    Update camedeon.py

--- camedeon.py
+++ camedeon.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-API_GOOGLE_MAPS = st.secrets["API_GOOGLE_MAPS"]
+API_GOOGLE_MAPS = os.environ['API_GOOGLE_MAPS']
 
 @st.dialog("Répondre à l'invitation")
 def show_contact_form():
